f77018ae574e0cb9a838cadd2c77fa0a323273c4
rafiferdos <rafiferdos@gmail.com>
2025-11-18 17:18:59 +0600

    chore: add @expo/vector-icons dependency and update HomeScreen to include navigation to CameraScreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,35 @@
-import { Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import { Pressable, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <>
-      <View
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      >
-        <Text style={{fontSize: 30}}>Home Screen</Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontSize: 30 }}>Home Screen</Text>
+        <Link href='/camera' asChild>
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 20,
+              justifyContent: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 20,
+                padding: 10,
+                backgroundColor: "lightgray",
+                marginTop: 20,
+                borderRadius: 50,
+              }}
+            >
+              Go to Camera
+              <MaterialIcons name='photo-camera' size={24} color='black' />
+            </Text>
+          </Pressable>
+        </Link>
       </View>
     </>
   );
